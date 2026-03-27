edf2b91a213827d41c6dcddd4ec459b8502391ab
The-Everesth <jesus.santillan.itid@unipolidgo.edu.mx>
2026-03-26 22:42:52 -0600

    Fix Firebase config for absolute credentials path on Render

--- config/firebase.php
+++ config/firebase.php
@@ -3,8 +3,15 @@
 return [
     // path to service account JSON
     'credentials' => env('FIREBASE_CREDENTIALS')
-        ? base_path(env('FIREBASE_CREDENTIALS'))
-        : (env('FIREBASE_CREDENTIALS_PATH') ? base_path(env('FIREBASE_CREDENTIALS_PATH')) : null),
+        ? (str_starts_with(env('FIREBASE_CREDENTIALS'), DIRECTORY_SEPARATOR)
+            ? env('FIREBASE_CREDENTIALS')
+            : base_path(env('FIREBASE_CREDENTIALS')))
+        : (env('FIREBASE_CREDENTIALS_PATH')
+            ? (str_starts_with(env('FIREBASE_CREDENTIALS_PATH'), DIRECTORY_SEPARATOR)
+                ? env('FIREBASE_CREDENTIALS_PATH')
+                : base_path(env('FIREBASE_CREDENTIALS_PATH')))
+            : null),
+
     // project id for Firestore REST calls
     'project_id' => env('FIREBASE_PROJECT_ID'),
 ];
